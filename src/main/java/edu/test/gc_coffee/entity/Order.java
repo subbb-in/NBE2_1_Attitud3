f1a4cdfdbe1228487f16e77da5de359041f6a7b0
subbb-in <48655473+subbb-in@users.noreply.github.com>
2024-09-06 12:44:25 +0900

    branch test

--- src/main/java/edu/test/gc_coffee/entity/Order.java
+++ src/main/java/edu/test/gc_coffee/entity/Order.java
@@ -1,4 +1,22 @@
 package edu.test.gc_coffee.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Order {
+
+    @Id
+    private String email;
+
+    private String orderId;
+
+    private String address;
+
+    private String postcode;
+
+    private String orderItems;
+
+    // private enum orderStatus;
+
 }
